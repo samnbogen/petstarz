7ab9f976db4fee8229d7bbb21202583292f565a3
Sam <samantha.bogen@edu.sait.ca>
2024-01-30 14:19:31 -0700

    footer

--- app/page.js
+++ app/page.js
@@ -1,21 +1,32 @@
 "use client";
 import { useState } from 'react';
 import Navbar from './navbar';
+import Footer from './footer';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex pt-36">
+      <header className="relative flex h-full pt-28">
+      <h1 className="z-10 pt-44 text-5xl text-center text-white font-bold">This is the home page.</h1>
+      <img src={'/cat.svg'} alt="description" className='z-0 absolute w-full h-72 object-cover brightness-50'/>
+        
+      </header>
+
+      <div className="z-10 max-w-5xl w-screen h-screen items-center justify-between lg:flex ">
         <h1 
-          className="text-3xl text-center text-black hover:text-light-gray"
+          className="text-3xl text-center hover:text-light-gray"
           style={{ fontFamily: 'Pacifico' }}>
             This is the home page.
         </h1>
-        <p className="text-3xl">
+        <p>
           This is some text.
         </p>
+        <p>
+          This is some more text.
+        </p>
       </div>
+      <Footer />
     </main>
   );
 }

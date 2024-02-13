755862ddce6ff2d3e07e67099ab688f6c986e230
DorianLaycock <119632701+DorianLaycock@users.noreply.github.com>
2024-02-13 13:16:22 -0700

    Added some functionality to forms

--- app/petcard/page.js
+++ app/petcard/page.js
@@ -1,31 +1,53 @@
 "use client";
 
+import { useState } from 'react';
 import Image from "next/image";
 import PetCardForm from "./Form.js";
 import Navbar from '/app/components/navbar.js';
-import FormNav from "./formnav.js";
+import Footer from '/app/components/footer.js';
 import RecordsForm from "./recordsform.js";
 import ContactsForm from "./contactsform.js";
 import PedigreeForm from "./pedigreeform.js";
 
 export default function Home() {
+
+  const [selectedForm, setSelectedForm] = useState('form1');
+
+  const handleFormChange = (form) => {
+    setSelectedForm(form);
+  };
+
   return (
     <main>
-        <Navbar />
-        <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="mt-10 z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <h1 
-                className="text-3xl text-center text-black hover:text-light-violet z-40"
-                style={{ fontFamily: 'Pacifico' }}>
-                    This is the petcard page.
-                </h1>
-            </div>
-            <FormNav /> {/* FormNav will allow the user to switch between forms with ease. For now, all forms are displayed for ease of development */}
-            <PetCardForm />
-            <RecordsForm />
-            <ContactsForm />
-            <PedigreeForm />
+      <Navbar />
+          <div className="flex min-h-screen flex-col p-24">
+              <div className="mt-10 z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                  <h1 
+                  className="text-3xl text-center text-black hover:text-light-violet z-40"
+                  style={{ fontFamily: 'Pacifico' }}>
+                      This is the petcard page.
+                  </h1>
+              </div>
+              <div class="h-10 flex flex-row w-2/5">
+                <div onClick={() => handleFormChange('form1')} class=" mr-1 p-1 rounded-t-lg w-1/4 bg-green hover:cursor-pointer">
+                    <h1 class="text-lg text-center text-white">General</h1>
+                </div>
+                <div onClick={() => handleFormChange('form2')}class=" mr-1 p-1 rounded-t-lg w-1/4 bg-gray hover:cursor-pointer">
+                    <h1 class="text-lg text-center text-white">Records</h1>
+                </div>
+                <div onClick={() => handleFormChange('form3')} class=" mr-1 p-1 rounded-t-lg w-1/4 bg-gray hover:cursor-pointer">
+                    <h1 class="text-lg text-center text-white">Contacts</h1>
+                </div>
+                <div onClick={() => handleFormChange('form4')} class="p-1 rounded-t-lg w-1/4 bg-gray hover:cursor-pointer">
+                    <h1 class="text-lg text-center text-white">Pedigree</h1>
+                </div>
+          </div>
+            {selectedForm === 'form1' && <PetCardForm />}
+            {selectedForm === 'form2' && <RecordsForm />}
+            {selectedForm === 'form3' && <ContactsForm />}
+            {selectedForm === 'form4' && <PedigreeForm />}
         </div>
+        <Footer />
     </main>
   );
 }

44a58c5bd1cf0b4b79e87b090bccf56cdc12d8c6
PatonFTW <patonFTWADED@gmail.com>
2024-02-22 15:15:44 -0700

    Updated main page (not finished)
    .

--- app/page.js
+++ app/page.js
@@ -1,23 +1,39 @@
-
 import Header from './components/header';
 import AuthProvider from './auth-provider';
 import Review from './review/review';
 
 export default function Home() {
   return (
-    <main>      
+    <div>
       <Header text="PetStarz" text2="The  Pawsitive  Online Source for Finding Pets."/>
-              
-      <div className="z-10 max-w-5xl w-screen h-screen items-center justify-between lg:flex ">
+      <main style={{ margin: '0px 300px' }}>      
         <h1 
-          className="text-3xl text-center hover:text-light-gray"
-          style={{ fontFamily: 'Pacifico' }}>
-            This is the home page.
+          className="text-5xl hover:text-light-gray"
+          style={{ fontFamily: 'Pacifico', fontWeight: 'bold', marginBottom: '40px', marginTop: '100px' }}>
+          Why Should You Choose PetStarz?
         </h1>
-        <p>
-          This is some text.
-        </p>
-      </div>
-    </main>
+        <h1 
+          className="text-3xl hover:text-light-gray"
+          style={{ fontFamily: 'Pacifico', fontWeight: 'bold', marginBottom: '20px' }}>
+          We are tackling the hard issues within the pet industry!
+        </h1>
+        <h1 
+          className="text-2xl hover:text-light-gray"
+          style={{ fontFamily: 'Pacifico', marginBottom: '20px' }}>
+          At PetStarz, we are a dynamic and innovative startup on a mission to revolutionize the pet industry. Our vision
+          is to create a groundbreaking SaaS online platform that transforms the way people source pets-ethically and safely.
+          We believe in the power of responsible pet ownership and the profound impact it has on the lives of both humans and
+          their pets.
+        </h1>
+        <h1 
+          className="text-2xl hover:text-light-gray"
+          style={{ fontFamily: 'Pacifico', marginBottom: '20px'  }}>
+          As PetStarz embarks on this journey, it's important to acknowledge that the challenges in the pet industry are complex 
+          and multifaceted and it will take time and dedication to ensure the well-being, ethical treatment, and responsible ownership 
+          of pets. However, our commitment to uniting various stakeholders, enhancing transparency, and providing valuable resources is 
+          a testament to our dedication to driving positive change.
+        </h1>
+      </main>
+    </div>
   );
 }

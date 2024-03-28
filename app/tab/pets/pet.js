ca930c030624617ad9fc83798265c453acd78cda
Sam <samantha.bogen@edu.sait.ca>
2024-03-28 16:21:47 -0600

    Update pet.js

--- app/tab/pets/pet.js
+++ app/tab/pets/pet.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import DOMPurify from 'dompurify';
+import Link from 'next/link';
 
-const PetCard = ({ photo, name, age }) => {
+const PetCard = ({ photo, name, age, _id }) => {
   const sanitizedPhoto = DOMPurify.sanitize(photo);
 
   return (
-    <div className="flex flex-col w-48 h-64 items-center justify-start border-green border-2 rounded-3xl">
-      
-      <div className="h-44 w-48 rounded-t-3xl flex items-center justify-center overflow-hidden">
-        <div
-          dangerouslySetInnerHTML={{ __html: sanitizedPhoto }}
-          />
+    <Link href={`/tab/pets/${_id}`}>
+      <div className="flex flex-col w-48 h-64 items-center justify-start border-green border-2 rounded-3xl">
+        <div className="h-44 w-48 rounded-t-3xl flex items-center justify-center overflow-hidden">
+          <div
+            dangerouslySetInnerHTML={{ __html: sanitizedPhoto }}
+            />
+        </div>
+        <div className="text-center p-2" >
+          <h1 className="text-2xl font-bold">{name}</h1>  
+          <h1 className="text-lg">Age: {age}</h1>
+        </div>
       </div>
-
-      <div className="text-center p-2" >
-        <h1 className="text-2xl font-bold">{name}</h1>  
-        <h1 className="text-lg">Age: {age}</h1>
-      </div>
-    </div>
+    </Link>
   );
 };
 
